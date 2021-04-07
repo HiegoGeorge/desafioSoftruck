b310cdb8810b55f17d9e205ac10ed284a04744a2
HiegoGeorge <hiego.oliveira@sga.pucminas.br>
2021-04-06 21:03:41 -0300

    Adicionando tags para melhor entendimento, do que esta sendo mostrado na página

--- js/script.js
+++ js/script.js
@@ -9,20 +9,19 @@ const carHeight = 151; // Altura 151
 const coluna = 151;
 const linha = 1;
 
-
 var atualizador ;
 var pos = 0;
 
-
+// funçao recebe os parametros, atualizando onde deve começar e terminar
 function updateFrame(inicio){
   const x = inicio;
 
-  if(atualizador == pos){
+  if(atualizador == pos){ 
      pos = ++pos;  
         console.log("valor do pos antes de " + pos);     
                 
     }else if(pos == x) {   
-         clearInterval (limpa); 
+         clearInterval (intervalo); 
       }else {
           var coluna = atualizador * carWidth ;
           img.style.marginLeft = -coluna + 'px'; // anda apenas na coluna 
@@ -31,11 +30,11 @@ function updateFrame(inicio){
   }
               
     
-function desenhar(){     
-  
+function desenhar(){   
        
  }
- var limpa = setInterval(function(){
+ // 
+ var intervalo = setInterval(function(){
   atualizar();
   },200);
 
@@ -43,8 +42,8 @@ function desenhar(){
    desenhar();
   }
 
-
-  function moveRight(){
+  // Função pega posição inicial que o carro esta na direita ate o final antes de mudar de posição 
+  function moveRight(){ 
     for(var i = 78; i<=104; i++){
       atualizador = i;
       updateFrame(i);
@@ -52,8 +51,10 @@ function desenhar(){
    console.log("valor do for " + i);
    
     }
+    initMap(-23.962782,-46.278824)
   }
   
+  // Função pega apenas a posição inicial da esquerda ate a final da esquerda
   function moveLeft(){
     for(var i = 20; i<=50; i++){
       atualizador = i;
@@ -61,23 +62,38 @@ function desenhar(){
       
    console.log("valor do for " + i);
     }
+  
+  initMap(-23.962701,-46.276151);
   }
 
+  // função movimento para cima
   function moveUp(){
     for(var i = 1; i<=10; i++){
       atualizador = i;
       updateFrame(i);
    console.log("valor do for " + i);
     }
+    initMap(-23.962278,-46.273007);
   }
-
+// função movimento para baixo
   function moveDonw(){
     for(var i = 50; i<=75; i++){
       atualizador = i;
       updateFrame(i);
+      
       console.log("valor do for " + i);
     }
+    initMap(-19.92818,-43.95562);
+  }
+  
+ // funçao que faz a integraçao com google maps
+  function initMap(x,y) {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+      center: {lat:x, lng:y},
+      zoom: 16
+    });
+   
   }
 
-  
  
